86f2e5179702402462441a4dda3187fef223a170
Umar <umar.hussain2924@gmail.com>
2024-03-24 01:19:23 +0500

    first commit

---
+++ app/components/Color.jsx
@@ -0,0 +1,25 @@
+// pages/index.js
+import { useState, useEffect } from 'react';
+
+const Home = () => {
+  const [backgroundColor, setBackgroundColor] = useState('#ffffff');
+
+  useEffect(() => {
+    document.body.style.backgroundColor = backgroundColor;
+  }, [backgroundColor]);
+
+  const changeBackgroundColor = () => {
+    const colors = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#00ffff", "#ff00ff", "#000000", "#ffffff"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    setBackgroundColor(randomColor);
+  };
+
+  return (
+    <div>
+      <button onClick={changeBackgroundColor}>Change Background Color</button>
+      {/* You can add other components here */}
+    </div>
+  );
+};
+
+export default Home;
